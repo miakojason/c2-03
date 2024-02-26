ac7ac811153232931a66f5959e66ba857a46ca6d
miakojason <miakojason@gmail.com>
2024-02-26 18:36:08 +0800

    處理最新文章

---
+++ front/pop.php
@@ -0,0 +1,21 @@
+<fieldset>
+    <legend>目前位置:首頁>最新文章區</legend>
+    <table>
+        <tr>
+            <th>標題</th>
+            <th>內容</th>
+            <th></th>
+        </tr>
+        <?php
+$news=$News->all();
+foreach($news as $new){
+    
+}
+        ?>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
+</fieldset>
